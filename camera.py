58900b9749232d34fbe9ba47d181da1cec0b3098
Hardiv Harshakumar <hardivhk@gmail.com>
2021-03-20 09:52:41 +0000

    Remove images

--- camera.py
+++ camera.py
@@ -9,7 +9,7 @@ video_count = 0
 camera.rotation = 0
 # camera.resolution = (2592, 1944) - maximum
 # camera.resolution = (64, 64) - minimum
-camera.resolution = (64, 64)
+camera.resolution = (256, 256)
 camera.framerate = 15
 # camera.image_effect = 'film' - grayscale
 
@@ -26,7 +26,7 @@ def reset():
 
 def take_photo(time_delay=1):
     global image_count
-    filename = '/home/pi/_code/Python/ExoGlove/assets/testing/Simage%s.jpg' % image_count
+    filename = '/home/pi/_code/Python/ExoGlove/assets/testing/image%s.jpg' % image_count
     # filename = '/home/pi/Pictures/image%s.jpg' % image_count
     camera.capture(filename)
     sleep(time_delay)

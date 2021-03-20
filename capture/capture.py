ca462105b0507197dbc33bf7a2d5865b567e4ad5
Hardiv Harshakumar <hardivhk@gmail.com>
2021-03-20 09:48:11 +0000

    Typo

--- capture/capture.py
+++ capture/capture.py
@@ -1,7 +1,7 @@
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir) # gping to the parent directory
+sys.path.append(parentdir) # going to the parent directory
 
 from camera import*
 

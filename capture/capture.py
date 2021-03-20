23f0efdd9b4218d9424ccdd9c7992470a34d3073
Hardiv Harshakumar <hardivhk@gmail.com>
2021-03-20 08:43:09 +0000

    Update capture.py

--- capture/capture.py
+++ capture/capture.py
@@ -11,7 +11,7 @@ GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled
 # low (off)
 
-count = 0
+count = 0  # Test
 while True:  # Run forever
     if GPIO.input(10) == GPIO.HIGH:
         print("Taking photo: ", count)

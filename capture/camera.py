9dae8d781a93e245c995ae2dfa64457663ca2bf3
Hardiv Harshakumar <hardivhk@gmail.com>
2021-04-04 07:47:09 +0100

    Update capture

--- capture/camera.py
+++ capture/camera.py
@@ -1,22 +1,25 @@
-from picamera import PiCamera
+import os
+import sys
 from time import sleep
+from picamera import PiCamera
 
+# File path Setup
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)  # going to the parent directory
+print("Went to parent directory", parentdir)
+
+
+# Camera Setup
 camera = PiCamera()
 image_count = 0
 video_count = 0
-
-# Setup
 camera.rotation = 0
 # camera.resolution = (2592, 1944) - maximum
 # camera.resolution = (64, 64) - minimum
 camera.resolution = (256, 256)
 camera.framerate = 15
 # camera.image_effect = 'film' - grayscale
-
-# Set transparency
-camera.start_preview(alpha=0)
-sleep(5)
-camera.stop_preview()
 
 
 def reset():
@@ -26,7 +29,7 @@ def reset():
 
 def take_photo(time_delay=1):
     global image_count
-    filename = '/home/pi/_code/Python/ExoGlove/assets/testing/image%s.jpg' % image_count
+    filename = 'assets/testing/image%s.jpg' % image_count
     # filename = '/home/pi/Pictures/image%s.jpg' % image_count
     camera.capture(filename)
     sleep(time_delay)
@@ -39,4 +42,10 @@ def record_video(time_delay):
     camera.start_recording('video%s.h264' % video_count)
     sleep(time_delay)
     camera.stop_recording()
+    camera.stop_preview()
+
+
+if __name__ == '__main__':
+    camera.start_preview(alpha=0)  # alpha sets transparency
+    sleep(5)
     camera.stop_preview()
--- capture/capture.py
+++ capture/capture.py
@@ -1,12 +1,8 @@
-import os, sys
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir) # going to the parent directory
+import camera
+import RPi.GPIO as GPIO
 
-from capture.camera import*
 
-import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled
 # low (off)
@@ -14,6 +10,6 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input
 count = 0  # Test
 while True:  # Run forever
     if GPIO.input(10) == GPIO.HIGH:  # Test 2
-        print("Taking photo: ", count)
-        take_photo()
+        print(f'Taking photo: {count}')
+        print(f'Photo saved in location {camera.take_photo(0.1)}')
         count += 1

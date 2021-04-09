9e7111c8390a40c75ba33e2a012deec80fbda999
Hardiv Harshakumar <hardivhk@gmail.com>
2021-04-09 13:35:56 +0100

    Update capture.py

--- capture/capture.py
+++ capture/capture.py
@@ -1,15 +1,21 @@
 import camera
 import RPi.GPIO as GPIO
 
+pushbutton = 18
+
+
+def buttonPressed(pin_number):
+    return GPIO.input(pin_number) == GPIO.HIGH
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled
+GPIO.setup(pushbutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled
 # low (off)
 
 count = 0  # Test
 while True:  # Run forever
-    if GPIO.input(10) == GPIO.HIGH:  # Test 2
+    if buttonPressed(pushbutton):  # Test 2
         print(f'Taking photo: {count}')
         print(f'Photo saved in location {camera.take_photo(0.05)}')
         count += 1

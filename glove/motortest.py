b9a8f778e1b9c67589fea7023c01f129b2ead4e2
Hardiv Harshakumar <hardivhk@gmail.com>
2021-04-09 21:05:09 +0100

    Update motortest.py

--- glove/motortest.py
+++ glove/motortest.py
@@ -3,8 +3,8 @@ kit = MotorKit()
 
 print("Motor 1")
 kit.motor1.throttle = 1.0
-print("Motor 2")
-kit.motor2.throttle = 1.0
+#print("Motor 2")
+#kit.motor2.throttle = 1.0
 #print("Motor 3")
 #kit.motor3.throttle = 1.0
 #print("Motor 4")

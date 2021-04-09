921147b96386d6e6776fd9343c7871510a6d9d04
Hardiv Harshakumar <hardivhk@gmail.com>
2021-04-09 22:26:39 +0100

    Update capture.py

--- capture/capture.py
+++ capture/capture.py
@@ -1,11 +1,16 @@
 import camera
 import RPi.GPIO as GPIO
 
-pushbutton = 18
+pushbutton = 17
+input = 4
 
 
 def buttonPressed(pin_number):
     return GPIO.input(pin_number) == GPIO.HIGH
+
+
+def switchOn(pin_number):
+    GPIO.output(pin_number, GPIO.HIGH)
 
 
 GPIO.setwarnings(False)
@@ -13,6 +18,7 @@ GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(pushbutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled
 # low (off)
 
+switchOn(input)
 count = 0  # Test
 while True:  # Run forever
     if buttonPressed(pushbutton):  # Test 2

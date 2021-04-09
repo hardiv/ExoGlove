c7ebf4c1838d841d467158034aee940db214fac6
Hardiv Harshakumar <hardivhk@gmail.com>
2021-04-09 21:37:23 +0100

    Update motortest.py

--- glove/motortest.py
+++ glove/motortest.py
@@ -2,7 +2,7 @@ from adafruit_motorkit import MotorKit
 kit = MotorKit()
 
 print("Motor 1")
-kit.motor1.throttle = 0.3
+kit.motor1.throttle = 0.5
 #print("Motor 2")
 #kit.motor2.throttle = 1.0
 #print("Motor 3")

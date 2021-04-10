5584046a51a99e74d8c7c294b0d1659bfc4b560f
Hardiv Harshakumar <hardivhk@gmail.com>
2021-04-10 01:22:20 +0100

    edit

--- glove/glove.py
+++ glove/glove.py
@@ -3,6 +3,17 @@
 import time
 import board
 from adafruit_motorkit import MotorKit
+import RPi.GPIO as GPIO
+import time
+
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# set GPIO Pins
+GPIO_TRIGGER = 27
+GPIO_ECHO = 22
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled
+GPIO.setup(18, GPIO.OUT)
 
 
 class Glove:
@@ -63,8 +74,25 @@ class Glove:
         self.bottom_thumb.throttle = multiplier*0.5
 
     def getDist(self):
-        sensor = self.ultrasonic
-        pass
+        # set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER, False)
+        StartTime = time.time()
+        StopTime = time.time()
+        # save StartTime
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
+        return distance
 
     def _haltAfter(self, num_secs):
         time.sleep(num_secs)
@@ -78,3 +106,12 @@ if __name__ == "__main__":
     glove.gripObject()
     print("Loosening")
     glove.relax()
+    try:
+        while True:
+            dist = distance()
+            print("Measured Distance = %.1f cm" % dist)
+            time.sleep(1)
+    # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
